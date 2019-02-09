35fae0cf36d699631f9bbc71c03dee8d5c9d2b30
Yann Armand <Yann@harakys.com>
2019-02-09 01:27:04 -0800

    fix test

--- importer/extract_infos_test.go
+++ importer/extract_infos_test.go
@@ -6,7 +6,7 @@ func TestGetCreationDate(t *testing.T) {
 	fname := "../testAssets/testImage.jpeg"
 
 	date := GetCreationDate(fname)
-	if !(date.Year() == 2018 && date.Month() == 6 && date.Day() == 24) {
+	if !(date.Year() == 2018 && date.Month() == 6 && date.Day() == 25) {
 		t.Error("Incorect date\nexpected: 2018-06-25 ...\ngot:", date)
 	}
 }
