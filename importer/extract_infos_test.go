d20c73fed6196447125ba56ed7df287aa8b0cb61
yann ARMAND <yann@harakys.com>
2019-02-10 22:51:43 -0800

    fix the test

--- importer/extract_infos_test.go
+++ importer/extract_infos_test.go
@@ -5,6 +5,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+// boo
 var _ = Describe("ExtractInfo", func() {
 	Describe("GetCreationDate", func() {
 		It("extract the creation date properly", func() {
@@ -12,7 +13,7 @@ var _ = Describe("ExtractInfo", func() {
 			date := GetCreationDate(fname)
 			Expect(date.Year()).To(Equal(2018))
 			Expect(int(date.Month())).To(Equal(6))
-			Expect(date.Day()).To(Equal(24))
+			Expect(date.Day()).To(Equal(25))
 		})
 	})
 })
