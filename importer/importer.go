af3651afc48dc4dc27f7aaefcc6d06500b2bdbc7
yann ARMAND <yarmand@yammer-inc.com>
2019-09-29 22:09:14 -0700

    lets do importer a bit like context.

--- importer/importer.go
+++ importer/importer.go
@@ -4,12 +4,33 @@ import (
 	"context"
 )
 
+// Importer is responsible to feed a chain of processors
+type Importer struct {
+	processor Processor
+}
+
 // Processor is a function that process an image read from disk
 type Processor func(ctx context.Context, filename string)
 
-// ImportFiles get a list of files and run processor on them.
-func ImportFiles(fileNames []string, process Processor) {
-	for _, fileName := range fileNames {
-		process(nil, fileName)
+// ImportFile improt a file by aplying the processor
+func (importer Importer) ImportFile(fileName string) {
+	importer.processor(context.Background(), fileName)
+}
+
+func nothing(ctx context.Context, filename string) {
+
+}
+
+// NewImporter create an base importer that does nothing
+func NewImporter() (importer Importer) {
+	importer = Importer{processor: nothing}
+	return
+}
+
+// AddProcessor add a *Processor* to the chain of processors used by the *Importer*
+func (importer Importer) AddProcessor(processor Processor) {
+	importer.processor = func(ctx context.Context, filename string) {
+		importer.processor(ctx, filename)
+		processor(ctx, filename)
 	}
 }
--- importer/importer_test.go
+++ importer/importer_test.go
@@ -12,7 +12,7 @@ func TestImportFiles(t *testing.T) {
 	var processedFiles []string
 	files := []string{"a", "b", "c"}
 	process := func(ctx context.Context, f string) { processedFiles = append(processedFiles, f) }
-	ImportFiles(files, process)
+	ImportFile(files[0])
 	if !cmp.Equal(files, processedFiles) {
 		t.Errorf("processedFiles do not equal input files\n processFiles=%v\ninputFiles=%v", processedFiles, files)
 	}
