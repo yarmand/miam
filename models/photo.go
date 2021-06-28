5a09209653346cfadd5433828420e091ea2e3fb9
yann ARMAND <yarmand@yammer-inc.com>
2021-06-27 20:22:15 -0700

    adding capabilities to photo model

--- models/photo.go
+++ models/photo.go
@@ -16,19 +16,32 @@ const ( // iota is reset to 0
 	Keep   = iota // c2 == 2
 )
 
+// states
+// use to recover on massive image list processing
+const (
+	Importing = iota
+	Imported  = iota
+)
+
 // flags
 const (
-	ToEdit = 1 << iota // a == 1 (iota has been reset)
+	ImportToFinish = 1 << iota // a == 1 (iota has been reset)
+	ToEdit         = 1 << iota
+	HasRaw         = 1 << iota
+	IsACopy        = 1 << iota // when phot is a copy, the original ID is stored in the field OriginalPhoto
 )
 
 // Photo is used by pop to map your photos database table to your go code.
 type Photo struct {
-	ID        uuid.UUID `json:"id" db:"id"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
-	Path      string    `json:"path" db:"path"`
-	Rating    int       `json:"rating" db:"rating"`
-	Flags     int64     `json:"flags" db:"flags"`
+	ID            uuid.UUID `json:"id" db:"id"`
+	CreatedAt     time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at" db:"updated_at"`
+	Path          string    `json:"path" db:"path"`
+	JpegFilename  string    `json:"jpeg_filename" db:"jpeg_filename"`
+	RawFilename   string    `json:"raw_filename" db:"raw_filename"`
+	Rating        int       `json:"rating" db:"rating"`
+	Flags         int64     `json:"flags" db:"flags"`
+	OriginalPhoto uuid.UUID `json:"original_photo" db:"original_photo"` // the ID of original if this one is a copy
 }
 
 // String is not required by pop and may be deleted
